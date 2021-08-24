23d969719821f047532d7a6eaf13c31a3cf1c7b1
Rashad-Muntar <muntartoure@gmail.com>
2021-08-24 00:33:22 +0000

    add eslint and babel rc

---
+++ src/Components/Currency.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const Currency = () => {
+    return(
+        <h1>Currency</h1>
+    )
+}
+export default Currency
---
+++ src/Containers/CurrencyList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Currency from "../Components/Currency";
+
+const CurrencyList = () => {
+    return(
+        <div>
+            <Currency />
+        </div>
+    )
+}
+export default CurrencyList
