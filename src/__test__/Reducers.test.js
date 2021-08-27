820ee4bb2c5f762e0b3df49f84dd1f5ac5483bf8
Rashad-Muntar <muntartoure@gmail.com>
2021-08-27 21:25:36 +0000

    add test for reducers

--- src/__test__/Reducers.test.js
+++ src/__test__/Reducers.test.js
@@ -1,32 +1,26 @@
-import filterReducer from '../Reducers/GenreFilter';
-import moviesReducer from '../Reducers/Movies';
 import filterReducer from '../Reducers/FilterReducer'
 import cryptoReducer from '../Reducers/CryptoReducer'
 
 describe('Reducers', () => {
-  const FETCH_MOVIES = 'FETCH_MOVIES';
-  const CHANGE_FILTER = 'CHANGE_FILTER';
   const action = {
-    type: FETCH_MOVIES,
-    movies: [
-      { movie1: 'movie1', genre_ids: [1, 2, 3] },
-      { movie1: 'movie1', genre_ids: [0, 4, 3] },
-      { movie1: 'movie1', genre_ids: [6, 2, 5] },
-    ],
+    type: "FETCH-DATA",
+    data: {
+      coins: [
+
+      ]
+    },
   };
+
   const action2 = {
-    type: CHANGE_FILTER,
+    type: "CHANGE_FILTER",
     filter: 2,
   };
-  it('movies reducer has an initial state', () => {
-    expect(moviesReducer([], {})).toEqual([]);
-  });
 
-  it('movies reducer has movies in state', () => {
-    expect(moviesReducer([], action).length).toEqual(3);
+  it('crypto reducer has an initial state', () => {
+    expect(cryptoReducer([], {})).toEqual([]);
   });
 
   it('filter reducer returns a genre id', () => {
-    expect(filterReducer('All', action2)).toEqual(2);
+    expect(filterReducer('', action2)).toEqual(2);
   });
 });
