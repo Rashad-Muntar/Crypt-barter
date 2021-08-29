a32980bab9795f6816864a2a70e40144aefc81aa
Rashad-Muntar <muntartoure@gmail.com>
2021-08-29 15:38:54 +0000

    Add negative testing for crypto card

--- src/__test__/Crypto.test.js
+++ src/__test__/Crypto.test.js
@@ -2,16 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
 import Crypto from '../Components/Crypto';
-import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-console.error = jest.fn();
-
-beforeEach(() => {
-  console.error.mockClear();
-});
 
 
 describe('CryptoCard DOM', () => {
@@ -27,6 +17,20 @@ describe('CryptoCard DOM', () => {
       />,
     ).toJSON();
     expect(cryptoCard).toMatchSnapshot();
+  });
+
+  it('renders card content correctly', () => {
+    const cryptoCard = renderer.create(
+      <Crypto
+        name="name"
+        price={5}
+        icon="df"
+        rank={5}
+        perChange={5}
+        symbol="34"
+      />,
+    ).toJSON();
+    expect(cryptoCard.name).not.toBe("random");
   });
 });
 
--- src/__test__/FilterForm.test.js
+++ src/__test__/FilterForm.test.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import FilterForm from '../Components/FilterForm';
+// import Crypto from '../Components/Crypto'
 
 describe('Home', () => {
   beforeEach(() => {
@@ -12,4 +13,9 @@ describe('Home', () => {
     const tree = renderer.create(<FilterForm />).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  // it('Renders Filter form component', () => {
+  //   const tree = renderer.create(< >).toJSON();
+  //   expect(tree).not.toMatchSnapshot();
+  // });
 });
